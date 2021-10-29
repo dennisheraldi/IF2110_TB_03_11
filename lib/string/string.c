b6216756972773d83c472ade35300f5b80c0c973
davidkarelhp <13520154@std.stei.itb.ac.id>
2021-10-29 20:27:57 +0700

    Struktur folder

---
+++ lib/string/string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "../../ADT/wordmachine.h"
+
+Word concatenate(Word w1, Word w2) {
+    Word w;
+    int i, j;
+    for (i = 0; i < w1.length; i++) {
+        w.contents[i] = w1.contents[i];
+    }
+    for (j = 0; j < w2.length; j++) {
+        w.contents[i] = w2.contents[j];
+        i++;
+    }
+    w.length = w1.length + w2.length;
+    return w;
+}
+
+void wordToString(Word w, char * str) {
+    int i;
+    for (i = 0; i < w.length; i++) {
+        str[i] = w.contents[i];
+    }
+    str[w.length] = '\0';
+}
