20c176fae88586048c81b7c27502c8377ed34a27
Fachry Dennis Heraldi <fachrydennisheraldi@gmail.com>
2021-11-16 22:42:42 +0700

    Perbaiki bug buy.c dan memperbarui command run.bat

--- lib/BUY/buy.c
+++ lib/BUY/buy.c
@@ -3,7 +3,8 @@
 
 void buy(){
 
-    int pilihan;
+    int pilihan, input;
+    boolean valid;
 
     // Menampilkan list gadget yang dapat dibeli
     printf("Uang Anda sekarang: %d Yen\n", balance);
@@ -13,44 +14,54 @@ void buy(){
     printf("3. Pintu Kemana Saja (1500 Yen)\n");
     printf("4. Mesin Waktu (3000 Yen)\n");
     printf("Gadget mana yang ingin kau beli? (ketik 0 jika ingin kembali)\n");
-    printf("ENTER COMMAND: "); scanf("%d", &pilihan);
-    
-    if (isFullLS(boughtGadget)){
-        printf("Inventory penuh, tidak dapat membeli gadget.\n");
-    } else {
-        if (pilihan == 0){
-            // keluar
-        } else if ((pilihan == 1 && balance >= 800) || (pilihan == 2 && balance >= 1200) ||
-        (pilihan == 3 && balance >= 1500) || (pilihan == 4 && balance >= 3000)) {
-                ELMTLS(boughtGadget, (indexOfLS(boughtGadget, -999))) = pilihan;
-                switch (pilihan) {
-                    case 1: 
-                        printf("Kain Pembungkus Waktu berhasil dibeli!\n");
-                        balance -= 800;
-                        printf("Uang anda sekarang: %d", balance);
-                        break;
-                    case 2:
-                        printf("Senter Pembesar berhasil dibeli!\n");
-                        balance -= 1200;
-                        printf("Uang anda sekarang: %d", balance);
-                        break;
-                    case 3:
-                        printf("Pintu Kemana Saja berhasil dibeli!\n");
-                        balance -= 1500;
-                        printf("Uang anda sekarang: %d", balance);
-                        break;
-                    case 4: 
-                        printf("Mesin Waktu berhasil dibeli!\n");
-                        balance -= 3000;
-                        printf("Uang anda sekarang: %d", balance);
-                        break;
-                    default:
-                        break;
-                }
-                
-        } else {
-            printf("Uang tidak cukup untuk membeli gadget!\n");
+    do {
+        printf("ENTER COMMAND: ");
+        getCommand();
+        input = command[0] - '0';
+        valid = ((0 <= input) && (input <= 4));
+        if (!valid)
+        {
+            printf("Input tidak valid !\n");
         }
- 
+    } while (!valid);
+    if (input != 0) {
+        if (isFullLS(boughtGadget)){
+        printf("Inventory penuh, tidak dapat membeli gadget.\n");
+        } else {
+            if (pilihan == 0){
+                // keluar
+            } else if ((pilihan == 1 && balance >= 800) || (pilihan == 2 && balance >= 1200) ||
+            (pilihan == 3 && balance >= 1500) || (pilihan == 4 && balance >= 3000)) {
+                    ELMTLS(boughtGadget, (indexOfLS(boughtGadget, -999))) = pilihan;
+                    switch (pilihan) {
+                        case 1: 
+                            printf("Kain Pembungkus Waktu berhasil dibeli!\n");
+                            balance -= 800;
+                            printf("Uang anda sekarang: %d", balance);
+                            break;
+                        case 2:
+                            printf("Senter Pembesar berhasil dibeli!\n");
+                            balance -= 1200;
+                            printf("Uang anda sekarang: %d", balance);
+                            break;
+                        case 3:
+                            printf("Pintu Kemana Saja berhasil dibeli!\n");
+                            balance -= 1500;
+                            printf("Uang anda sekarang: %d", balance);
+                            break;
+                        case 4: 
+                            printf("Mesin Waktu berhasil dibeli!\n");
+                            balance -= 3000;
+                            printf("Uang anda sekarang: %d", balance);
+                            break;
+                        default:
+                            break;
+                    }
+                    
+            } else {
+                printf("Uang tidak cukup untuk membeli gadget!\n");
+            }
+        }
     }
+    
 }
--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 title Mobilita
 
-gcc *.c ADT/*.c lib/BUY/*.c lib/commandParser/*.c lib/DROP_OFF/*.c lib/MAP/*.c lib/MOVE/*.c lib/PICK_UP/*.c lib/string/*.c lib/TO_DO/*.c config/*.c -o bin -lm -fno-stack-protector
+gcc *.c ADT/*.c lib/BUY/*.c lib/commandParser/*.c lib/DROP_OFF/*.c lib/MAP/*.c lib/MOVE/*.c lib/PICK_UP/*.c lib/STRING_PROCESSOR/*.c lib/TO_DO/*.c config/*.c lib/HELP/*.c -o bin -lm -fno-stack-protector
 bin
 
 pause
