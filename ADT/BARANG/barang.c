6bd7aad8ee9d8c3a3e0b8bc2337cb3eebf5ab1a6
Nelsen Putra <16520272@std.stei.itb.ac.id>
2021-11-17 02:45:10 +0700

    Update include path in barang.c

--- ADT/BARANG/barang.c
+++ ADT/BARANG/barang.c
@@ -1,5 +1,5 @@
 #include "barang.h"
-#include "../IF2110_TB_11_03/config/initconfig.h"
+#include "../../config/initconfig.h"
 
 boolean EQBarang(Barang a, Barang b){
     return (ORDER_TIME(a)==ORDER_TIME(b)) && (PICKUP_LOC(a)==PICKUP_LOC(b)) && (DROPOFF_LOC(a)==DROPOFF_LOC(b)) && (TYPE(a)==TYPE(b));
