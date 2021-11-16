8f1b9eb89a497b581fd59841b4363e3736a25a10
Nelsen Putra <16520272@std.stei.itb.ac.id>
2021-11-17 00:58:09 +0700

    Add IN PROGRESS command

---
+++ lib/IN_PROGRESS/in_progress.c
@@ -0,0 +1,30 @@
+#include "../../config/initconfig.h"
+
+void displayInProgress() {
+  Address p;
+  int count;
+  
+  // Inisialisasi
+  p = FIRST(inProgress);
+  count = 1;
+
+  // Menampilkan isi list inProgress
+  while (p != NULL) {
+    printf("%d. ", count);
+    // Mengecek tipe barang
+    if (TYPE(INFO(p)) == 'N') {
+      printf("Normal Item ");
+    } else if (TYPE(INFO(p)) == 'H') {
+      printf("Heavy Item ");
+    } else if (TYPE(INFO(p)) == 'P') {
+      printf("Perishable Item ");
+    }
+
+    // Menampilkan drop-off location
+    printf("(Tujuan: %c)", DROPOFF_LOC(INFO(p)));
+    printf("\n");
+
+    p = NEXT(p);
+    count++;
+  }
+}
---
+++ lib/IN_PROGRESS/in_progress.h
@@ -0,0 +1,5 @@
+#include "../../config/initconfig.h"
+
+void displayInProgress();
+/* Menampilkan pesanan yang sedang dikerjakan. Pesanan ditampilkan 
+   secara berurutan sesuai dengan pesanan yang terakhir masuk. */
