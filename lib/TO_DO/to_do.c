33ed7fe51730f21a0694ef334a0a941f2c2fb503
davidkarelhp <13520154@std.stei.itb.ac.id>
2021-11-17 22:59:09 +0700

    Update

--- lib/TO_DO/to_do.c
+++ lib/TO_DO/to_do.c
@@ -4,29 +4,33 @@ void displayList()
 {
     // printf("panjang lengthPrio: %d",lengthPrio(antrianPesanan));
     // printf("panjang antrian: %d",length(antrian));
-    int length = lengthPrio(antrianPesanan);
-    Address p = FIRST(antrian);
-    int count = 0;
-    while (p != NULL)
-    {
-        Barang temp = INFO(p);
-        printf("%d. ",(count+1));
-        printf("%c -> %c ",PICKUP_LOC(temp),DROPOFF_LOC(temp));
+    if (isEmpty(antrian)) {
+        printStringGreen("\nTidak ada pesanan yang harus diantar\n\n");
+    } else {
+        int length = lengthPrio(antrianPesanan);
+        Address p = FIRST(antrian);
+        int count = 0;
+        while (p != NULL)
+        {
+            Barang temp = INFO(p);
+            printf("%d. ",(count+1));
+            printf("%c -> %c ",PICKUP_LOC(temp),DROPOFF_LOC(temp));
 
-        if (TYPE(temp) == 'N')
-        {
-            printf("(Normal Item)");
+            if (TYPE(temp) == 'N')
+            {
+                printf("(Normal Item)");
+            }
+            else if (TYPE(temp) == 'H')
+            {
+                printf("(Heavy Item)");
+            }
+            else if (TYPE(temp) == 'P')
+            {
+                printf("(Perishable Item)");
+            }
+            printf("\n");
+            p = NEXT(p);
+            count++;
         }
-        else if (TYPE(temp) == 'H')
-        {
-            printf("(Heavy Item)");
-        }
-        else if (TYPE(temp) == 'P')
-        {
-            printf("(Perishable Item)");
-        }
-        printf("\n");
-        p = NEXT(p);
-        count++;
     }
 }
