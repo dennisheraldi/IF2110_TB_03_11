4b5797e18d826703daaca5ef4e206c55b84c781e
Nelsen Putra <16520272@std.stei.itb.ac.id>
2021-11-18 14:18:21 +0700

    Update driver for matrix module

--- ADT/MATRIX/driver_matrix.c
+++ ADT/MATRIX/driver_matrix.c
@@ -35,13 +35,13 @@ int main() {
     readMatrix(&M2, nb, nk);
 
     nl; printf("PERINDEKSAN MATRIKS"); nl;
-    printf("First Index Baris : %d", GetFirstIdxBrs(M1)); nl;
-    printf("First Index Kolom : %d", GetFirstIdxKol(M1)); nl;
+    printf("First Index Baris : 0"); nl;
+    printf("First Index Kolom : 0"); nl;
     printf("Last Index Baris : %d", getLastIdxRow(M1)); nl;
     printf("Last Index Kolom : %d\n", getLastIdxCol(M1));
     printf("Masukkan i: "); scanf("%d", &i);
     printf("Masukkan j: "); scanf("%d", &j);
-    if (IsIdxEffM(M1, i, j)) {
+    if (isIdxEffM(M1, i, j)) {
         printf("Indeks efektif\n");
     } else {
         printf("Indeks tidak efektif\n");
@@ -59,15 +59,26 @@ int main() {
         printf("Matriks M1 dan M2 tidak sama ukuran efektifnya\n");
     }
     
-    nl; printf("SYMMETRY"); nl;
+    nl; printf("JENIS MATRIKS"); nl;
     if (isSquare(M1)) {
         printf("Matriks M1 adalah matriks bujur sangkar\n");
+    } else {
+        printf("Matriks M1 bukan matriks bujur sangkar\n");
     }
-    else printf("Matriks M1 bukan matriks bujur sangkat\n");
     if (isSymmetric(M1)) {
         printf("Matriks M1 simetris\n");
     } else {
         printf("Matriks M1 tidak simetris\n");
+    } 
+    if (isIdentity(M1)) {
+        printf("Matriks M1 adalah matriks identitas\n");
+    } else {
+        printf("Matriks M1 bukan matriks identitas\n");
+    }
+    if (isSparse(M1)) {
+        printf("Matriks M1 adalah matriks jarang\n");
+    } else {
+        printf("Matriks M1 bukan matriks jarang\n");
     } 
 
     nl; printf("PRINTING MATRIKS"); nl;
@@ -87,6 +98,11 @@ int main() {
     transpose(&M3);
     printf("M3\n");
     displayMatrix(M3); nl;
+
+    nl; printf("DETERMINANT"); nl;
+    printf("Determinan Matriks M1: %f", determinant(M1)); nl;
+    printf("Determinan Matriks M2: %f", determinant(M2)); nl;
+    printf("Determinan Matriks M3: %f", determinant(M3)); nl;
 
     float exeKiller;
     printf("\n\n(End of Program - Enter non-whitespace character to exit)");
